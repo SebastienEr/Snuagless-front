4df2048c63092089f5ff4e345e657c840ff474b6
Louie <foxx999@hotmail.fr>
2024-03-06 11:08:32 +0100

    reducer + modal ugly open

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.scss";
 import Head from "next/head";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import user from "../reducers/user";
+
+const store = configureStore({
+  reducer: { user },
+});
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Radio Snuagless</title>
         <link
@@ -12,7 +19,7 @@ function App({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
