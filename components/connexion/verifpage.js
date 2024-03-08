55d3ffc158b1bfa0d68d6db9e8ad79200f559e39
Louie <foxx999@hotmail.fr>
2024-03-08 17:02:10 +0100

    lola connexion frontend

--- components/connexion/verifpage.js
+++ components/connexion/verifpage.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import styles from "../connexion/verifpage.module.css";
-import { useDispatch } from 'react-redux'; // Ajout de l'import
+import { useDispatch } from "react-redux"; // Ajout de l'import
 
 // Import des actions de redux
 import { login } from "../../reducers/user";
@@ -10,8 +10,7 @@ function VerifPage() {
   const router = useRouter();
   const dispatch = useDispatch(); // Initialisation du dispatch
 
-  const [verificationStatus, setVerificationStatus] = useState('');
-  
+  const [verificationStatus, setVerificationStatus] = useState("");
 
   useEffect(() => {
     if (router.query.token) {
@@ -20,30 +19,40 @@ function VerifPage() {
   }, [router.query.token]);
 
   const verifyEmail = (token) => {
-    
-    fetch(`http://localhost:3000/users/verify-email?token=${token}`, { method: 'GET' })
-      .then(response => response.json())
-      .then(data => {
+    /* a comment for commit reason */
+    fetch(`http://localhost:3000/users/verify-email?token=${token}`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
         if (data.success) {
           dispatch(login({ token: token, username: data.username }));
-          setVerificationStatus('Votre email a été vérifié avec succès. Vous allez être redirigé...');
+          setVerificationStatus(
+            "Votre email a été vérifié avec succès. Vous allez être redirigé..."
+          );
           setTimeout(() => {
-            router.push('/');
+            router.push("/");
           }, 3000);
         } else {
-          setVerificationStatus('Échec de la vérification de l\'email. Le lien peut être expiré ou invalide.');
+          setVerificationStatus(
+            "Échec de la vérification de l'email. Le lien peut être expiré ou invalide."
+          );
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Erreur lors de la vérification de l'email :", error);
-        setVerificationStatus('Erreur lors de la connexion au serveur. Veuillez réessayer plus tard.');
+        setVerificationStatus(
+          "Erreur lors de la connexion au serveur. Veuillez réessayer plus tard."
+        );
       });
   };
 
   return (
     <div className={styles["verif-page"]}>
       <h1 className={styles["verif-page-h1"]}>Vérification de l'email</h1>
-      <p className={styles["verif-page-p"]}>{verificationStatus || 'En attente de la vérification de l\'email...'}</p>
+      <p className={styles["verif-page-p"]}>
+        {verificationStatus || "En attente de la vérification de l'email..."}
+      </p>
     </div>
   );
 }
