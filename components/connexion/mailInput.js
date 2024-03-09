9548553d16faca48e3ccc38e2db66eeb84cf3cf7
Sébastien <pellangeon@gmail.com>
2024-03-09 11:35:44 +0100

    push front

--- components/connexion/mailInput.js
+++ components/connexion/mailInput.js
@@ -1,25 +1,25 @@
-import styles from "../connexion/connexion.module.css";
-import React from "react";
+// import styles from "../connexion/connexion.module.css";
+// import React from "react";
 
-function MailInput ({value, onChange, required}){
-    const handleChange = (e) => {
-        const email = e.target.value;
-        onChange(email);
-    };
+// function MailInput ({value, onChange, required}){
+//     const handleChange = (e) => {
+//         const email = e.target.value;
+//         onChange(email);
+//     };
 
-    return (
-        <div className={styles.inputBox}>
-            <div className={styles.emailBox}>
-          <input
-            type="email"
-            required={required}
-            onChange={handleChange}
-            value={value}
-          />
-          <label>Mail</label>
-          </div>
-        </div>
-      );
-}
+//     return (
+//         <div className={styles.inputBox}>
+//             <div className={styles.emailBox}>
+//           <input
+//             type="email"
+//             required={required}
+//             onChange={handleChange}
+//             value={value}
+//           />
+//           <label>Mail</label>
+//           </div>
+//         </div>
+//       );
+// }
 
-export default MailInput;
+// export default MailInput;
